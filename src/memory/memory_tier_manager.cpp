374df12e6ad628ab1472d926ffb1b0df225d565c
scrallex <56850857+scrallex@users.noreply.github.com>
2025-06-28 17:32:19 -0500

    Include necessary headers and declare helper function

--- src/memory/memory_tier_manager.cpp
+++ src/memory/memory_tier_manager.cpp
@@ -1,5 +1,7 @@
 #include "memory/memory_tier_manager.hpp"
 #include "memory/types.h"
+#include "quantum/types.h"        // For MemoryTierEnum definition
+#include "quantum/pattern.h"      // For QuantumPattern definition
 
 #include "compat/cuda_common.h"
 

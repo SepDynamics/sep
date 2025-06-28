e67a5e3fb9dbf3425835988310a5c57a196b6d3c
scrallex <56850857+scrallex@users.noreply.github.com>
2025-06-28 12:55:59 -0500

    Fix build issues

--- src/core/allocation_metrics.cpp
+++ src/core/allocation_metrics.cpp
@@ -3,15 +3,14 @@
 #include "core/prometheus_exporter.h"
 #include "core/types.h"
 
+namespace sep::metrics {
 namespace {
 Counter g_allocation_failures{"allocation_failures_total", "Total memory allocation failures"};
-bool    registered = false;
+bool registered = false;
 }  // namespace
 
-Counter& allocationFailures()
-{
-    if (!registered)
-    {
+Counter& allocationFailures() {
+    if (!registered) {
         PrometheusExporter::instance().registerCounter(&g_allocation_failures);
         registered = true;
     }

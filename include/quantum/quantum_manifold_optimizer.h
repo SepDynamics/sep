1502e11b81b5bda30a661477b5a3e9cd8575a2f0
scrallex <alex@scrallex.com>
2025-06-29 00:01:33 -0500

    89

--- include/quantum/quantum_manifold_optimizer.h
+++ include/quantum/quantum_manifold_optimizer.h
@@ -1,6 +1,10 @@
 // quantum_manifold_optimizer.h
 #pragma once
 
+// Include core types first for config definitions
+#include "core/types.h"
+
+// Other includes
 #include "quantum/pattern_evolution_bridge.h"
 #include <algorithm>
 #include <array>
@@ -26,7 +30,6 @@
 #include "compat/cuda.h"
 #include "compat/cuda_runtime.h"
 #include "compat/cufft.h"
-#include "core/types.h"
 #include "memory/memory_tier_manager.hpp"
 #include "memory/types.h"
 #include "quantum/qbsa.h"
@@ -45,24 +48,13 @@ using QuantumPattern = ::sep::quantum::manifold::QuantumPattern;
 using ManifoldQuantumState = ::sep::quantum::manifold::ManifoldQuantumState;
 using ::sep::quantum::QFHResult;
 using ::sep::quantum::QuantumProcessorQFH;
-// Configuration structures from the core configuration module use
-// capitalised names (e.g. CUDAConfig). Import them with matching
-// casing to avoid "does not name a type" errors.
-using CoreCUDAConfig = ::sep::config::CUDAConfig;
-using CoreAPIConfig = ::sep::config::APIConfig;
-using CoreLogConfig = ::sep::config::LogConfig;
-using CoreAnalyticsConfig = ::sep::config::AnalyticsConfig;
-
-// Forward declare configuration structs used by QuantumManifoldOptimizer
-struct CudaConfig;
-struct ApiConfig;
 
 class QuantumManifoldOptimizer {
 public:
     struct Config {
         MemoryTierEnum tier{MemoryTierEnum::STM};
-        CUDAConfig cuda;
-        APIConfig api;
+        CudaConfig cuda;
+        ApiConfig api;
         LogConfig log;
         double base_resonance_frequency{0.42};
         double convergence_threshold{0.001};
@@ -175,9 +167,9 @@ struct SemanticConfig {
 
 struct ManifoldConfig {
     SemanticConfig semantic;
-    CudaConfig cuda;
-    ApiConfig api;
-    LogConfig log;
+    ::sep::config::CudaConfig cuda;
+    ::sep::config::ApiConfig api;
+    ::sep::config::LogConfig log;
     AnalyticsConfig analytics;
 };
 

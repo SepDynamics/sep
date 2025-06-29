19e843aa7f08e9c920331dc9d9a1b0de5974019d
scrallex <56850857+scrallex@users.noreply.github.com>
2025-06-28 23:38:11 -0500

    Clean up QuantumManifoldOptimizer

--- include/quantum/quantum_manifold_optimizer.h
+++ include/quantum/quantum_manifold_optimizer.h
@@ -50,13 +50,10 @@ using ::sep::quantum::QuantumProcessorQFH;
 // import them with different casing which resulted in a large number of
 // "does not name a type" compilation errors.  Import them with the
 // correct names instead.
-using ::sep::config::CUDAConfig;
-using ::sep::config::APIConfig;
+using ::sep::config::CudaConfig;
+using ::sep::config::ApiConfig;
 using ::sep::config::LogConfig;
 using ::sep::config::AnalyticsConfig;
-using ::sep::config::APIConfig;
-using ::sep::config::CUDAConfig;
-using ::sep::config::LogConfig;
 
 class QuantumManifoldOptimizer {
 public:
@@ -79,7 +76,6 @@ public:
         QuantumState optimized_state{};
         std::vector<float> optimized_values;
         std::string error_message;
-        QuantumState optimized_state{};
     };
 
     struct OptimizationTarget {

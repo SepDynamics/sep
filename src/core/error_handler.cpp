fc9fce09e16c1c5fadc2253a58df56e9aa7ecfe7
scrallex <56850857+scrallex@users.noreply.github.com>
2025-07-01 14:41:58 -0500

    chore: remove stray fix comment

--- src/core/error_handler.cpp
+++ src/core/error_handler.cpp
@@ -3,7 +3,7 @@
 
 #include <functional> // Required for std::function
 
-// Standard Library Includes // Fix: Added comment
+// Standard Library Includes 
 namespace sep::core {
 using ::sep::Error;
 using ::sep::shim::vector;

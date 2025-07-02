11a45c26734ace520ff18ee21c86ab4277e60ff0
scrallex <56850857+scrallex@users.noreply.github.com>
2025-07-02 05:19:39 -0500

    Use Boost Asio for socket adaptors

--- include/crow/socket_adaptors.h
+++ include/crow/socket_adaptors.h
@@ -18,20 +18,20 @@
 #include "compat/shim.h"
 #include "crow/logging.h"
 #include "crow/asio_isolation.h"
+#include <memory>
 
 namespace crow {
-    // Use our stub asio implementation
-    using tcp = asio_stub::ip::tcp;
+    using tcp = asio::ip::tcp;
 
     /// A wrapper for the asio::ip::tcp::socket
     struct SocketAdaptor {
         using context = void;
-        SocketAdaptor(asio_stub::io_context& io_context, context*) : socket_() {}
-
-        asio_stub::io_context& get_io_context() { 
-            static asio_stub::io_context ctx;
-            return ctx;
+        explicit SocketAdaptor(asio::io_context& io_context, context*) : socket_(io_context), io_ctx_(&io_context) {
+            boost::system::error_code ignore_ec;
+            socket_.close(ignore_ec);
         }
+
+        asio::io_context& get_io_context() { return *io_ctx_; }
 
         tcp::socket& raw_socket() { return socket_; }
         tcp::socket& socket() { return socket_; }
@@ -40,55 +40,71 @@ namespace crow {
 
         error_code close() {
             error_code ec;
-            socket_.close(ec);
+            if (!socket_.is_open()) {
+                ec = boost::asio::error::not_connected;
+            } else {
+                socket_.close(ec);
+            }
             return ec;
         }
 
         error_code shutdown_readwrite() {
             error_code ec;
-            socket_.shutdown(asio_stub::socket_base::shutdown_both, ec);
+            if (!socket_.is_open()) {
+                ec = boost::asio::error::not_connected;
+            } else {
+                socket_.shutdown(boost::asio::socket_base::shutdown_both, ec);
+            }
             return ec;
         }
 
         error_code shutdown_write() {
             error_code ec;
-            socket_.shutdown(asio_stub::socket_base::shutdown_send, ec);
+            if (!socket_.is_open()) {
+                ec = boost::asio::error::not_connected;
+            } else {
+                socket_.shutdown(boost::asio::socket_base::shutdown_send, ec);
+            }
             return ec;
         }
 
         error_code shutdown_read() {
             error_code ec;
-            socket_.shutdown(asio_stub::socket_base::shutdown_receive, ec);
+            if (!socket_.is_open()) {
+                ec = boost::asio::error::not_connected;
+            } else {
+                socket_.shutdown(boost::asio::socket_base::shutdown_receive, ec);
+            }
             return ec;
         }
 
         template <typename F>
         void start(F f) {
-            f(error_code());
+            f(error_code{});
         }
 
         tcp::socket socket_;
+        asio::io_context* io_ctx_;
     };
 
     #ifdef CROW_ENABLE_SSL
     struct SSLAdaptor {
-        using context = asio_stub::ssl::context;
-        using ssl_socket_t = asio_stub::ssl::stream<tcp::socket>;
-        
-        SSLAdaptor(asio_stub::io_context& io_context, context* ctx) : 
-            ssl_socket_(new ssl_socket_t(io_context, *ctx)) {}
+        using context = asio::ssl::context;
+        using ssl_socket_t = asio::ssl::stream<tcp::socket>;
+
+        SSLAdaptor(asio::io_context& io_context, context* ctx) :
+            ssl_socket_(std::make_unique<ssl_socket_t>(io_context, *ctx)) {}
 
         ssl_socket_t& socket() { return *ssl_socket_; }
-        tcp::socket& raw_socket() { 
-            static tcp::socket socket;
-            return socket;
-        }
+        tcp::socket& raw_socket() { return ssl_socket_->next_layer(); }
         tcp::endpoint remote_endpoint() { return raw_socket().remote_endpoint(); }
-        bool is_open() { return ssl_socket_ ? raw_socket().is_open() : false; }
+        bool is_open() { return ssl_socket_ && raw_socket().is_open(); }
 
         error_code close() {
             error_code ec;
-            if (is_open()) {
+            if (!is_open()) {
+                ec = boost::asio::error::not_connected;
+            } else {
                 raw_socket().close(ec);
             }
             return ec;
@@ -96,36 +112,42 @@ namespace crow {
 
         error_code shutdown_readwrite() {
             error_code ec;
-            if (is_open()) {
-                raw_socket().shutdown(asio_stub::socket_base::shutdown_both, ec);
+            if (!is_open()) {
+                ec = boost::asio::error::not_connected;
+            } else {
+                raw_socket().shutdown(boost::asio::socket_base::shutdown_both, ec);
             }
             return ec;
         }
 
         error_code shutdown_write() {
             error_code ec;
-            if (is_open()) {
-                raw_socket().shutdown(asio_stub::socket_base::shutdown_send, ec);
+            if (!is_open()) {
+                ec = boost::asio::error::not_connected;
+            } else {
+                raw_socket().shutdown(boost::asio::socket_base::shutdown_send, ec);
             }
             return ec;
         }
 
         error_code shutdown_read() {
             error_code ec;
-            if (is_open()) {
-                raw_socket().shutdown(asio_stub::socket_base::shutdown_receive, ec);
+            if (!is_open()) {
+                ec = boost::asio::error::not_connected;
+            } else {
+                raw_socket().shutdown(boost::asio::socket_base::shutdown_receive, ec);
             }
             return ec;
         }
 
-        asio_stub::io_context& get_io_context() { 
-            static asio_stub::io_context ctx;
-            return ctx;
+        asio::io_context& get_io_context() {
+            return static_cast<asio::io_context&>(ssl_socket_->get_executor().context());
         }
 
         template <typename F>
         void start(F f) {
-            ssl_socket_->async_handshake(asio_stub::ssl::server, [f](const error_code& ec) { f(ec); });
+            ssl_socket_->async_handshake(asio::ssl::stream_base::server,
+                                         std::move(f));
         }
 
         std::unique_ptr<ssl_socket_t> ssl_socket_;

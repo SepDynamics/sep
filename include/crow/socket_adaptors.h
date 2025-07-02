616cf7d5badb19a64d6cb53fc8e600de85e0b1db
scrallex <56850857+scrallex@users.noreply.github.com>
2025-07-02 04:50:58 -0500

    feat: integrate boost asio in socket adaptors

--- include/crow/socket_adaptors.h
+++ include/crow/socket_adaptors.h
@@ -14,51 +14,61 @@
 #endif
 #endif
 
-// Include our own headers
+// Include our own headers and Boost.ASIO
 #include "compat/shim.h"
 #include "crow/logging.h"
 #include "crow/asio_isolation.h"
+#include <boost/asio.hpp>
+#ifdef CROW_ENABLE_SSL
+#include <boost/asio/ssl.hpp>
+#endif
 
 namespace crow {
-    // Use our stub asio implementation
+    // Alias Boost.ASIO TCP type
     using tcp = asio_stub::ip::tcp;
 
     /// A wrapper for the asio::ip::tcp::socket
     struct SocketAdaptor {
         using context = void;
-        SocketAdaptor(asio_stub::io_context& io_context, context*) : socket_() {}
+        explicit SocketAdaptor(asio_stub::io_context& io_context, context*)
+            : socket_(io_context), io_(io_context) {}
 
-        asio_stub::io_context& get_io_context() { 
-            static asio_stub::io_context ctx;
-            return ctx;
-        }
+        asio_stub::io_context& get_io_context() { return io_; }
 
         tcp::socket& raw_socket() { return socket_; }
         tcp::socket& socket() { return socket_; }
-        tcp::endpoint remote_endpoint() { return socket_.remote_endpoint(); }
+        tcp::endpoint remote_endpoint() {
+            error_code ec;
+            auto ep = socket_.remote_endpoint(ec);
+            return ec ? tcp::endpoint() : ep;
+        }
         bool is_open() { return socket_.is_open(); }
 
         error_code close() {
             error_code ec;
+            if (!is_open()) {
+                ec = make_error_code(asio_stub::error::not_connected);
+                return ec;
+            }
             socket_.close(ec);
             return ec;
         }
 
         error_code shutdown_readwrite() {
             error_code ec;
-            socket_.shutdown(asio_stub::socket_base::shutdown_both, ec);
+            socket_.shutdown(tcp::socket::shutdown_both, ec);
             return ec;
         }
 
         error_code shutdown_write() {
             error_code ec;
-            socket_.shutdown(asio_stub::socket_base::shutdown_send, ec);
+            socket_.shutdown(tcp::socket::shutdown_send, ec);
             return ec;
         }
 
         error_code shutdown_read() {
             error_code ec;
-            socket_.shutdown(asio_stub::socket_base::shutdown_receive, ec);
+            socket_.shutdown(tcp::socket::shutdown_receive, ec);
             return ec;
         }
 
@@ -68,22 +78,24 @@ namespace crow {
         }
 
         tcp::socket socket_;
+        asio_stub::io_context& io_;
     };
 
     #ifdef CROW_ENABLE_SSL
     struct SSLAdaptor {
         using context = asio_stub::ssl::context;
         using ssl_socket_t = asio_stub::ssl::stream<tcp::socket>;
-        
-        SSLAdaptor(asio_stub::io_context& io_context, context* ctx) : 
-            ssl_socket_(new ssl_socket_t(io_context, *ctx)) {}
+
+        SSLAdaptor(asio_stub::io_context& io_context, context* ctx)
+            : ssl_socket_(std::make_unique<ssl_socket_t>(io_context, *ctx)) {}
 
         ssl_socket_t& socket() { return *ssl_socket_; }
-        tcp::socket& raw_socket() { 
-            static tcp::socket socket;
-            return socket;
+        tcp::socket& raw_socket() { return ssl_socket_->next_layer(); }
+        tcp::endpoint remote_endpoint() {
+            error_code ec;
+            auto ep = raw_socket().remote_endpoint(ec);
+            return ec ? tcp::endpoint() : ep;
         }
-        tcp::endpoint remote_endpoint() { return raw_socket().remote_endpoint(); }
         bool is_open() { return ssl_socket_ ? raw_socket().is_open() : false; }
 
         error_code close() {
@@ -97,7 +109,7 @@ namespace crow {
         error_code shutdown_readwrite() {
             error_code ec;
             if (is_open()) {
-                raw_socket().shutdown(asio_stub::socket_base::shutdown_both, ec);
+                raw_socket().shutdown(tcp::socket::shutdown_both, ec);
             }
             return ec;
         }
@@ -105,7 +117,7 @@ namespace crow {
         error_code shutdown_write() {
             error_code ec;
             if (is_open()) {
-                raw_socket().shutdown(asio_stub::socket_base::shutdown_send, ec);
+                raw_socket().shutdown(tcp::socket::shutdown_send, ec);
             }
             return ec;
         }
@@ -113,19 +125,18 @@ namespace crow {
         error_code shutdown_read() {
             error_code ec;
             if (is_open()) {
-                raw_socket().shutdown(asio_stub::socket_base::shutdown_receive, ec);
+                raw_socket().shutdown(tcp::socket::shutdown_receive, ec);
             }
             return ec;
         }
 
-        asio_stub::io_context& get_io_context() { 
-            static asio_stub::io_context ctx;
-            return ctx;
+        asio_stub::io_context& get_io_context() {
+            return ssl_socket_->get_executor().context();
         }
 
         template <typename F>
         void start(F f) {
-            ssl_socket_->async_handshake(asio_stub::ssl::server, [f](const error_code& ec) { f(ec); });
+            ssl_socket_->async_handshake(asio_stub::ssl::stream_base::server, [f](const error_code& ec) { f(ec); });
         }
 
         std::unique_ptr<ssl_socket_t> ssl_socket_;

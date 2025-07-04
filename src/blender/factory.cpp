f77110d62e6dd308b9c6eddb03a9d483e586da42
scrallex <56850857+scrallex@users.noreply.github.com>
2025-07-04 07:04:48 -0500

    Fix std namespace issues in blender sources

--- src/blender/factory.cpp
+++ src/blender/factory.cpp
@@ -1,5 +1,8 @@
 #include <cstring> // Added for C string functions
 #include <ctime>   // Added for C time functions
+#include <string> // Required for std::string
+
+using namespace std; // For string type
 #include "blender/factory.h"
 #include "blender/bridge.h"
 #include "blender/types.h"

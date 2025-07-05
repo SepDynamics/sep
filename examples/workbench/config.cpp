6dee39aa98396c4424d428522d700d6c5e5312b9
scrallex <56850857+scrallex@users.noreply.github.com>
2025-07-05 17:59:22 -0500

    Add neural demo with config

--- examples/workbench/config.cpp
+++ examples/workbench/config.cpp
@@ -90,6 +90,15 @@ bool Config::load(const std::filesystem::path& path) {
             }
         };
 
+        // Neural demo config
+        auto& neural = json["demos"]["neural"];
+        neural_ = {
+            neural["neuron_count"].get<int>(),
+            neural["threshold"].get<float>(),
+            neural["decay"].get<float>(),
+            neural["connection_weight"].get<float>()
+        };
+
         // Renderer config
         auto& renderer = json["renderer"];
         auto& cycles = renderer["cycles"];
@@ -174,6 +183,14 @@ bool Config::save(const std::filesystem::path& path) const {
                 {"connection_opacity", memory_garden_.visualization.connection_opacity},
                 {"pattern_scale", memory_garden_.visualization.pattern_scale}
             }}
+        };
+
+        // Neural demo config
+        json["demos"]["neural"] = {
+            {"neuron_count", neural_.neuron_count},
+            {"threshold", neural_.threshold},
+            {"decay", neural_.decay},
+            {"connection_weight", neural_.connection_weight}
         };
 
         // Renderer config

2539048d56ec53b3b30f475b3e121dfa6a79050d
scrallex <56850857+scrallex@users.noreply.github.com>
2025-07-05 23:41:25 -0500

    Update manifold config types

--- include/quantum/manifold_config.h
+++ include/quantum/manifold_config.h
@@ -3,8 +3,8 @@
 #include "quantum/quantum_manifold_optimizer.h"
 
 namespace sep::quantum::manifold {
-    extern MemoryConfig memory;
-    extern QuantumConfig quantum;
+    extern ::sep::config::MemoryThresholdConfig memory;
+    extern ::sep::config::QuantumThresholdConfig quantum;
     extern ::sep::config::CudaConfig cuda;
     extern ::sep::config::APIConfig api;
 }

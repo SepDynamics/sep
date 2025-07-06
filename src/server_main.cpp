3764c13b2896b70eb91e2dc068a6b00e71df11bf
scrallex <56850857+scrallex@users.noreply.github.com>
2025-07-06 15:36:22 -0500

    refactor: decouple renderer from headless server

--- src/server_main.cpp
+++ src/server_main.cpp
@@ -1,9 +1,7 @@
 #include "api/server.h"
 #include "core/manager.h"
 #include "core/logging.h"
-#ifdef SEP_HAS_CYCLES
-#include "blender/cycles_renderer.h"
-#endif
+#include <cstddef>
 #include <iostream>
 #include <memory>
 
@@ -17,21 +15,10 @@ int main(int argc, char** argv) {
     const auto& api_cfg = cfg_manager.getAPIConfig();
     logger->info("Configuration loaded. API will run on port {}.", api_cfg.port);
 
-#ifdef SEP_HAS_CYCLES
-    // Only create the renderer when Cycles is enabled
-    std::unique_ptr<sep::blender::ccl::CyclesRenderer> renderer = nullptr;
-    renderer = std::make_unique<sep::blender::ccl::CyclesRenderer>();
-    if (renderer->initialize() != sep::SEPResult::SUCCESS) {
-        logger->critical("Failed to initialize Cycles Renderer!");
-        return 1;
-    }
-    
-    // Pass the renderer to the server when Cycles is enabled
-    sep::api::SEPApiServer server(api_cfg, renderer.get());
-#else
-    // Pass nullptr when Cycles is disabled
+
+    // The headless server does not require a renderer. Pass nullptr
+    // to satisfy the API when compiled with rendering support.
     sep::api::SEPApiServer server(api_cfg, nullptr);
-#endif
 
     logger->info("Server object created.");
 

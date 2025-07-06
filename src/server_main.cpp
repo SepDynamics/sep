2b51e887fa824fb9f7fef513dab96bf0beecae3b
scrallex <56850857+scrallex@users.noreply.github.com>
2025-07-06 15:36:30 -0500

    Separate server and workbench

--- src/server_main.cpp
+++ src/server_main.cpp
@@ -1,9 +1,6 @@
 #include "api/server.h"
 #include "core/manager.h"
 #include "core/logging.h"
-#ifdef SEP_HAS_CYCLES
-#include "blender/cycles_renderer.h"
-#endif
 #include <iostream>
 #include <memory>
 
@@ -17,21 +14,7 @@ int main(int argc, char** argv) {
     const auto& api_cfg = cfg_manager.getAPIConfig();
     logger->info("Configuration loaded. API will run on port {}.", api_cfg.port);
 
-#ifdef SEP_HAS_CYCLES
-    // Only create the renderer when Cycles is enabled
-    std::unique_ptr<sep::blender::ccl::CyclesRenderer> renderer = nullptr;
-    renderer = std::make_unique<sep::blender::ccl::CyclesRenderer>();
-    if (renderer->initialize() != sep::SEPResult::SUCCESS) {
-        logger->critical("Failed to initialize Cycles Renderer!");
-        return 1;
-    }
-    
-    // Pass the renderer to the server when Cycles is enabled
-    sep::api::SEPApiServer server(api_cfg, renderer.get());
-#else
-    // Pass nullptr when Cycles is disabled
     sep::api::SEPApiServer server(api_cfg, nullptr);
-#endif
 
     logger->info("Server object created.");
 

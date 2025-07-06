8a8d66e45e207f61aee5239537bd59fa97eafd0d
scrallex <56850857+scrallex@users.noreply.github.com>
2025-07-05 23:02:58 -0500

    Add manifold config header

---
+++ include/quantum/manifold_config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "core/types.h"
+
+namespace sep::quantum::manifold {
+    extern MemoryConfig memory;
+    extern QuantumConfig quantum;
+    extern ::sep::config::CudaConfig cuda;
+    extern ::sep::config::APIConfig api;
+}
+
--- src/quantum/manifold_config.cpp
+++ src/quantum/manifold_config.cpp
@@ -1,4 +1,4 @@
-#include "quantum/quantum_manifold_optimizer.h"
+#include "quantum/manifold_config.h"
 
 namespace sep::quantum::manifold {
 

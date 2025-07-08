028c40b863790506c690d2612be2b9792b96e4e9
scrallex <alex@scrallex.com>
2025-07-07 21:49:13 -0500

    a

--- install.sh
+++ install.sh
@@ -79,3 +79,6 @@ if apt-cache show gcc-14 >/dev/null 2>&1; then
   sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-14 100
   sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-14 100
 fi
+
+mkdir -p /sep
+mount --bind /workspace/sep /sep 

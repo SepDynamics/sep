ba5137cdcc306be7c847707d306c9d94c2f3bb06
scrallex <56850857+scrallex@users.noreply.github.com>
2025-07-08 09:06:15 -0500

    Remove stray using directive

--- src/audio/pipewire_capture.cpp
+++ src/audio/pipewire_capture.cpp
@@ -47,7 +47,7 @@ extern "C" {
 namespace sep {
 namespace audio {
 
-using namespace std; // Add this to fix namespace issues
+
 
 
 sep::audio::PipeWireCapture::PipeWireCapture() : stream_events_{} {}

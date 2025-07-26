bfac0e30e2715e3b985be8235f716894c2092fa0
scrallex <56850857+scrallex@users.noreply.github.com>
2025-07-26 00:32:28 -0500

    Add backtesting suite panel and improve analysis script

--- src/apps/workbench/tabs/backend_tab_controller.h
+++ src/apps/workbench/tabs/backend_tab_controller.h
@@ -37,6 +37,7 @@ private:
     void renderBacktesterPanel();
     void renderOrderManagementPanel();
     void renderTradeHistoryPanel();
+    void renderBacktestingSuite();
     void updateTradeHistory();
 
     void handleDataLoad();
